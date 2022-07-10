279ff32385b589ccacaa7baa998b8dab395553c6
b3rking <ishimweeschylebersy@gmail.com>
2022-07-10 19:22:17 +0200

    add exception handler and set rules for endpoints

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,10 @@
 
 namespace App\Exceptions;
 
+// use GuzzleHttp\Psr7\Request;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -35,6 +38,17 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function(NotFoundHttpException $e, Request $request) {
+            // add the check to see if it's an api response
+            if ($request->is('api/*')) {
+                return response([
+                    'success' => 'false',
+                    'error' => [
+                        'endpoint not existing'    
+                    ]], 404);
+            }
         });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,21 +4,27 @@ use App\Http\Controllers\EleveController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+/*===============================================
+/ API ROUTING - b3rking
+/
+/ routes to the api will start with the version and then the endpoint
+/ ex: /v1/students/store
+/*===============================================*/
+
+
+// main endpoint!
+Route::get('/', function() {
+    return response([
+        'success' => 'true',
+        'data' => [
+            'message' => 'welcome on our API. this is the main endpoint for a list of all api endpoint and usage refer to the docs available at the adress bellow.',
+            'docs-link' => '/api-docs']]);
 });
 
-//apiEleve
 
-Route::get('eleve_api', [EleveController::class, 'apiEleve'])->name('eleve_api');
+// versioning, all endpoint for the first version will go in this group.
+Route::prefix('v1')->group(function() {
+    Route::get('eleve_api', [EleveController::class, 'apiEleve'])->name('eleve_api');
+});
